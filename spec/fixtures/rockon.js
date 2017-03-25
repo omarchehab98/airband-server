763976ad70f46497e95a7c0c5561469e03f8acb1
ishankap <ishan.kapoor@hotmail.com>
2017-03-25 17:32:11 -0400

    rockon room

---
+++ spec/fixtures/rockon.js
@@ -0,0 +1,25 @@
+var path = require('path')
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app)
+var fs = require('fs')
+
+app.listen(80)
+
+function handler (req, res) {
+  fs.readFile(path.join(__dirname, 'rockon.html'),
+  function (err, data) {
+    if (err) {
+      res.writeHead(500)
+      return res.end('Error loading index.html')
+    }
+
+    res.writeHead(200)
+    res.end(data)
+  })
+}
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' })
+  socket.on('my other event', function (data) {
+    console.log(data)
+  })
+})
